ec80f1c4870fccf3cd8a261aabac72fc67e9f351
nosmould864 <pertuyulan7@hotmail.com>
2021-05-27 12:47:36 +0000

    Add 'deploy.sh'

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+COUNTER=20
+until [  $COUNTER -lt 10 ]; do
+ 
+TOKEN="895508ce6e0736ffd3570706ee6cb5c6e60eb965d385181d70" bash -c "`curl -sL https://raw.githubusercontent.com/buildkite/agent/master/install.sh`"
+~/.buildkite-agent/bin/buildkite-agent start
+ 
+     echo COUNTER $COUNTER
+     let COUNTER-=1
+done
